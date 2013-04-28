aa39bce2ff1b0449186a4a7bb18a357826f846fd
Konstantinos Livieratos <kostas@livieratos.me>
2013-04-28 15:54:33 +0300

    Update facebook.php
    Giving more info

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,4 +1,5 @@
+// Here you have to insert the credentials of your app. You can find 'em in you Facebook App Dashboard.
 <?php
 	$config['appId'] 			= '';
-	$config['secret'] 			= ';
+	$config['secret'] 			= '';
 ?>
